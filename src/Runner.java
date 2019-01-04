6b821a0994d67cb99eb781f209495bb23b188e3e
Ricky <rchu9700@bths.edu>
2019-01-03 23:41:18 -0500

    fedefgee

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,22 @@
 public class Runner {
     public static void main(String[] args) {
-        int[] list = SortingUtil.randIntArr(100);
+        int[] testArr = SortingUtil.randIntArr(1000);
+        System.out.print("Before: ");
+        System.out.println();
+        for (int num:testArr) {
+            System.out.print(num + " ");
+            System.out.println();
+        }
         long time = System.nanoTime();
-        SortingUtil.bubble(list);
-        for (int cur : list) {
-            System.out.println(cur);
+        SortingUtil.bubbleSort(testArr);
+        System.out.print("After: ");
+        System.out.println();
+        for (int num:testArr) {
+            System.out.print(num + " ");
+            System.out.println();
         }
         time = System.nanoTime() - time;
+        System.out.println("It took " + SortingUtil.bubbleSort.swaps + " swaps to sort this list.");
         System.out.println("Time taken: " + time + " nanoseconds");
     }
 }
--- src/SortingUtil.java
+++ src/SortingUtil.java
@@ -1,29 +1,30 @@
 public class SortingUtil {
+
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    public static void bubble(int[] arr) {
+    public static void bubbleSort(int[] arr) {
         int swaps = -1;
-        while (swaps != -1) {
+        while (swaps != 0) {
             swaps = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] > arr[i+1]) {
-                    swap(arr, i , i+1);
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
                     swaps++;
                 }
             }
         }
-        System.out.println("It took you " + swaps + "times.");
     }
 
     public static int[] randIntArr(int count) {
         int[] randomArr = new int[count];
         for (int i = 0; i < randomArr.length; i++)  {
-            randomArr[i] = (int) (Math.random() * 101);
+            randomArr[i] = (int) (Math.random() * 10001);
         }
         return randomArr;
     }
+
 }
